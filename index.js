27d32663ef9197bba83ec860646f008d49a2f888
abd-005 <abd.me7@gmail.com>
2025-11-08 21:17:56 +0600

    Client Connected to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,10 +8,30 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://smartdbUser:Yo7Gxk2kFetGZWbc@cluster0.ljdyez8.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res)=>{
     res.send('Smart server is running')
 })
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, ()=>{
     console.log(`Smart server is running on port: ${port}`)
