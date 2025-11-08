b2bfaf4c3b0762048496a43462db9df40acb155b
abd-005 <abd.me7@gmail.com>
2025-11-08 22:14:16 +0600

    CRUD Implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -25,6 +25,57 @@ app.get('/', (req, res)=>{
 async function run() {
   try {
     await client.connect();
+
+    const database = client.db("smart_user");
+    const smartCollections = database.collection("smartCollections");
+
+    // GET || Read
+    app.get('/users', async(req,res)=>{
+        const cursor = smartCollections.find({});
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // Get specific
+
+    app.get('/users:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await smartCollections.findOne(query);
+
+        res.send(result);
+    })
+
+    // POST || Create
+    app.post('/users', async(req,res)=>{
+        const newProduct = req.body;
+
+        const result = await smartCollections.insertOne(newProduct);
+
+        res.send(result);
+    })
+
+    // DELETE
+    
+    app.delete('users/:id', async(req,res)=>{
+        const id = req.params.id;
+
+        const query = {_id: new ObjectId(id)};
+        const result = await smartCollections.deleteOne(query);
+        res.send(result)
+    }) 
+
+    //UPDATE
+    app.patch('/users/:id', async(req, res)=>{
+        const id = req.params.id;
+        const updatedUser = req.body; 
+        const query = {_id: new ObjectId(id)};
+        const update = {
+        $set:{updatedUser}
+        }
+        const result= await smartCollections.updateOne(query, update);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
